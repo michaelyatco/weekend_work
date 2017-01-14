81860f51db313cc2f09370daef3745bd9b69b478
michaelyatco <mjyatco@gmail.com>
2017-01-14 16:20:24 -0500

    Add files via upload
    Here is my homework for week 1

---
+++ weekend_work/algo/reverse_a_string.rb
@@ -0,0 +1,25 @@
+# Complete the method called reverse_a_string that accepts a string as a parameter and 
+# returns the reverse. The one caveat: Don't use the reverse method that already
+# comes with Ruby!
+
+def reverse_a_string(string)
+  half_length = string.length / 2
+  half_length.times {|i| string[i], string[-i-1] = string[-i-1], string[i] }
+  string
+end
+
+# Driver code - don't touch anything below this line.
+puts "TESTING reverse_a_string..."
+puts
+
+result = reverse_a_string("abcde")
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == "edcba"
+  puts "PASS!"
+else
+  puts "F"
+end
